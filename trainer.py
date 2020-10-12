3c0d7fa2e48c5c551cb64322deb321c95708c61c
Louise P <cpieri@student.42.fr>
2020-10-12 17:38:41 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,5 +1,9 @@
 import sys
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+
+
+scaler = MinMaxScaler()
 
 
 def parse_file(file_name):
@@ -11,40 +15,41 @@ def parse_file(file_name):
             continue
         tab = [int(km), int(price)]
         matrice.append(tab)
-    matrice = MinMaxScaler().fit_transform(matrice)
+    matrice = scaler.fit_transform(matrice)
     x = matrice[:, 0]
     y = matrice[:, 1]
+    plt.scatter(x, y)
     return x, y
 
 
 def model_function(theta_0, theta_1, x):
-    estimate_price = (theta_0 + (theta_1 * x))
+    estimate_price = theta_0 + (theta_1 * x)
     return estimate_price
 
 
 def cost_function(old_theta_0, old_theta_1, x, y):
     tmp_t0 = tmp_t1 = 0.0
     for i in range(len(x)):
-        tmp_t0 += (model_function(old_theta_0, old_theta_1, x[i]) - y[i])
-        tmp_t1 += ((model_function(old_theta_0, old_theta_1, x[i]) - y[i]) * x[i])
+        tmp_t0 += (old_theta_0 + (old_theta_1 * x[i]) - y[i])
+        tmp_t1 += (old_theta_0 + (old_theta_1 * x[i]) - y[i]) * x[i]
     derivative_t0 = (1 / len(x)) * tmp_t0
-    derivative_t1 = (1 / len(y)) * tmp_t1
+    derivative_t1 = (1 / len(x)) * tmp_t1
     return derivative_t0, derivative_t1
 
 
 def gradient_descent(file_name):
     (x, y) = parse_file(file_name)
-    t0 = t1 = 0.0
-    ratio = 0.001
-    for i in range(1000):
-        (derivative_t0, derivative_t1) = cost_function(t0, t1, x, y)
-        t0 -= ratio * derivative_t0
-        t1 -= ratio * derivative_t1
-    return t0, t1
-
-
-# def export_theta(final_t0, final_t1):
-#     print(final_t0, final_t1)
+    theta_0 = theta_1 = 0.0
+    learning_rate = 0.035
+    for i in range(2000):
+        (derivative_t0, derivative_t1) = cost_function(theta_0, theta_1, x, y)
+        theta_0 -= derivative_t0 * learning_rate
+        theta_1 -= derivative_t1 * learning_rate
+    # t0 /= scaler.scale_[0]
+    # t1 /= scaler.scale_[1]
+    print(theta_0, theta_1)
+    plt.plot([theta_0, theta_1], color="pink")
+    return theta_0, theta_1
 
 
 if __name__ == '__main__':
@@ -53,8 +58,4 @@ if __name__ == '__main__':
     else:
         print("Starting the training")
         (t0, t1) = gradient_descent(sys.argv[1])
-        print(t0, t1)
-        val = input("Get km ")
-        test = model_function(t0, t1, float(val))
-        print(test)
-        # export_theta(t0, t1)
+        plt.show()
